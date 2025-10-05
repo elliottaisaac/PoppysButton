4dbbc7de94f74ea7d959c67c3f9f02d28722ccfb
elliottaisaac <49501453+elliottaisaac@users.noreply.github.com>
2025-10-05 12:49:34 -0400

    Create poppybutton

---
+++ messages.js
@@ -0,0 +1,76 @@
+
+let messages = [
+    "I love you",
+
+    "Ég elska þig",
+
+    "I've probably listened to your 'Future Days' cover at least 100 times. It's beautiful. Better than the original. Goosebumps everytime. ",
+    "Your pictures make me smile",
+
+    "You are so, so beautiful",
+
+    "You're so sexy. Like goddamn, baby. Sometimes I almost forget how hot you are because I'm so in love.<br><br>But then I remember like...oof.",
+
+    "I've never been able to picture myself as a parent. Like, couldn't even conceptualize what that would be like or feel like. But with you, I don't know... I think I can imagine it.",
+
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=-zpEMkKJd0M' target="_blank"style="font-size:500%";>&#9835; </a></button><br><sup>(4)</sup>`,
+
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=hq0rZ3IiyWw' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(3)</sup>`,
+
+     `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=6ydpSVvXE9k' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(2)</sup>`,
+
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=o3hHsVtqNFQ' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(1)</sup>`,
+
+    'You look really cute in pink',
+
+    "You mean everything to me",
+
+    "Can I call you Poppsi Max?",
+
+    "When's you're birthday? Mine is June 22nd",
+
+    "Do you have a favorite number? Mine is 22",
+
+    "This is too funny - when we were on the phone that one time, I had the idea that next time you tell me that you love me I'll surprise you by responding in Icelandic. The flaw in my plan was that I had no clue how to pronouce <i>ég elska þig líka</i> and I couldn't play any audio of how it's supposed to sound because you'd hear it. So I was Googling everything I could to try and figure it out, silently. Then you suddenly said 'I love you', and so I took my best shot at it. I had a big smile when you understood me.<br><br><img src='je elska theygh.jpg' style='width:250px'>",
+
+    "Our first hug is going to be wild",
+
+    "The thought of living in the UK is exciting. I think I'd fit in just fine. There must be so many trains. And I'd be like one of those people with a cool British accent, except the reverse. You get me, fam.",
+
+    'Get ready for some magic in 3... 2... 1...<div id="hearts-alpaca" class="hearts" style="padding:60px;"><div class="heart"></div><div class="heart"></div><div class="heart"></div><div class="heart"></div></div>',
+
+    '<span style="font-size:300%">&#128065; &#x1F474;&#x1F3FC; &infin; &#x1F334; &#x1FAF5; &#x1F590;</span>',
+
+    "How many people know how it feels to listen to a song someone wrote for them, specifically? Like, a really heartfelt song. Like the ones you've written for me. Probably not many... I'm lucky then because the only way I can think of to describe that feeling is that I imagine it's the closest an adult can get to being sung a lullabye.",
+
+    "I'd like to write you a song one day. But you'll have to sing it to yourself. Sorry.",
+
+    "I chose this font because its name is <a href='https://fonts.google.com/specimen/Poppins?preview.text=I%20love%20you' target='_blank'>Poppins</a>",
+    
+    "Mmmmwah",
+
+    "Why me?<br><br>Why rescue me where others drowned?",
+
+    '<div class="neonCont"><h1 class="neonText">Neon letters</h1></div>',
+
+    "Sometimes your music make me weepy. Like everything I've held in just starts leaking out.",
+
+    "I can't lose you.",
+
+    "I'm not sure you were kidding &#129300;<br><br><img src='husband.jpg' style='width:250px'>",
+];
+
+
+let names = [
+    "baby",
+    "baby!",
+    "honey",
+    "Poppy",
+    "dear",
+    "beautiful",
+    "angel",
+    "gorgeous",
+    "darling",
+    "my love",
+    "babe",
+]
