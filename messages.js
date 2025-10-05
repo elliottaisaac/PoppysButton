224260c086a7bd450b395af74988650d9583d7b9
elliottaisaac <49501453+elliottaisaac@users.noreply.github.com>
2025-10-05 13:42:29 -0400

    add content

--- messages.js
+++ messages.js
@@ -1,41 +1,48 @@
 
 let messages = [
-    "I love you",
+    "I love you.",
 
-    "Ég elska þig",
+    "Ég elska þig.",
 
     "I've probably listened to your 'Future Days' cover at least 100 times. It's beautiful. Better than the original. Goosebumps everytime. ",
-    "Your pictures make me smile",
 
-    "You are so, so beautiful",
+    "Your pictures make me smile.",
+
+    "You are so, so beautiful.",
 
     "You're so sexy. Like goddamn, baby. Sometimes I almost forget how hot you are because I'm so in love.<br><br>But then I remember like...oof.",
 
     "I've never been able to picture myself as a parent. Like, couldn't even conceptualize what that would be like or feel like. But with you, I don't know... I think I can imagine it.",
 
-    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=-zpEMkKJd0M' target="_blank"style="font-size:500%";>&#9835; </a></button><br><sup>(4)</sup>`,
-
     `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=hq0rZ3IiyWw' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(3)</sup>`,
 
-     `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=6ydpSVvXE9k' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(2)</sup>`,
+     `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=6ydpSVvXE9k' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(5)</sup>`,
 
     `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=o3hHsVtqNFQ' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(1)</sup>`,
 
-    'You look really cute in pink',
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=c1VNAUqwREY' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(4)</sup>`,
 
-    "You mean everything to me",
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=nt3Ggo1CE3g' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>(2)</sup>`,
+
+    `<button class="btn btn-pulse"><a href='https://www.youtube.com/watch?v=K5KAc5CoCuk' target="_blank" style="font-size:500%";>&#9835; </a></button><br><sup>Could you sing this?<br>(Don't do it, I'm just curious)</sup>`,
+
+    'You look really cute in pink.',
+
+    "You mean everything to me.",
 
     "Can I call you Poppsi Max?",
 
-    "When's you're birthday? Mine is June 22nd",
+    "When's you're birthday?<br>Mine is June 22nd.",
 
-    "Do you have a favorite number? Mine is 22",
+    "Do you have a favorite number?<br>Mine is 22.",
 
-    "This is too funny - when we were on the phone that one time, I had the idea that next time you tell me that you love me I'll surprise you by responding in Icelandic. The flaw in my plan was that I had no clue how to pronouce <i>ég elska þig líka</i> and I couldn't play any audio of how it's supposed to sound because you'd hear it. So I was Googling everything I could to try and figure it out, silently. Then you suddenly said 'I love you', and so I took my best shot at it. I had a big smile when you understood me.<br><br><img src='je elska theygh.jpg' style='width:250px'>",
+    "Did I get yourr colors right?<br><div id='colors'><div></div><div></div><div></div><div></div><div></div><div></div></div>",
 
-    "Our first hug is going to be wild",
+    "This is too funny - when we were on the phone that one time, I had the idea that next time you tell me that you love me I'll surprise you by responding in Icelandic. The flaw in my plan was that I had no clue how to pronouce <i>ég elska þig líka</i> and I couldn't play any audio of how it's supposed to sound because you'd hear it. So I was Googling everything I could to try and figure it out, silently. Then you suddenly said 'I love you', and so I took my best shot at it. I smiled when you understood me.<br><br><img src='je elska theygh.jpg' style='width:250px'>",
 
-    "The thought of living in the UK is exciting. I think I'd fit in just fine. There must be so many trains. And I'd be like one of those people with a cool British accent, except the reverse. You get me, fam.",
+    "Our first hug is going to be wild.",
+
+    "I think I'd fit in just fine in the UK. I'd be like one of those people with a cool British accent, except the reverse. You know?",
 
     'Get ready for some magic in 3... 2... 1...<div id="hearts-alpaca" class="hearts" style="padding:60px;"><div class="heart"></div><div class="heart"></div><div class="heart"></div><div class="heart"></div></div>',
 
@@ -45,9 +52,9 @@ let messages = [
 
     "I'd like to write you a song one day. But you'll have to sing it to yourself. Sorry.",
 
-    "I chose this font because its name is <a href='https://fonts.google.com/specimen/Poppins?preview.text=I%20love%20you' target='_blank'>Poppins</a>",
+    "I chose this font because its name is <a href='https://fonts.google.com/specimen/Poppins?preview.text=I%20love%20you' target='_blank'>Poppins</a>.",
     
-    "Mmmmwah",
+    "Mmmmwah!",
 
     "Why me?<br><br>Why rescue me where others drowned?",
 
@@ -71,6 +78,6 @@ let names = [
     "angel",
     "gorgeous",
     "darling",
-    "my love",
+    "love",
     "babe",
 ]
